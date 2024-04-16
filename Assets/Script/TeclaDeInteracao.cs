31356c1d00bcc37dbca38e7a29681707b8e1bb55
Golembr27 <142447072+Golembr27@users.noreply.github.com>
2024-04-16 13:00:34 -0300

    Movimentacao
    Arrumei a movimentação dos personagens

--- Assets/Script/TeclaDeInteracao.cs
+++ Assets/Script/TeclaDeInteracao.cs
@@ -5,19 +5,24 @@ using static UnityEngine.EventSystems.EventTrigger;
 
 public class TeclaDeInteracao : MonoBehaviour
 {
+    // Variável para controlar se o jogador está dentro do alcance da placa
+    private bool jogadorPerto = false;
+
     // Referência para o objeto de diálogo
     public GameObject dialogo;
 
     // Tecla para interagir
-    public KeyCode teclaDeInteracao = KeyCode.E;
+    public KeyCode teclaDeInteracao;
 
-    // Variável para controlar se o jogador está dentro do alcance da placa
-    private bool jogadorPerto = false;
+    private void Start()
+    {
+        teclaDeInteracao = KeyCode.E;
+    }
 
     void Update()
     {
         // Verificar se o jogador está perto e pressionou a tecla de interação
-        if (jogadorPerto && Input.GetKeyDown(teclaDeInteracao))
+        if (jogadorPerto == true && Input.GetKeyDown(teclaDeInteracao))
         {
             // Exibir o diálogo
             dialogo.SetActive(true);
@@ -25,22 +30,16 @@ public class TeclaDeInteracao : MonoBehaviour
     }
 
     // Este método é chamado quando o jogador entra no collider da placa
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Verificar se o jogador entrou no collider da placa
-        if (other.CompareTag("Player"))
-        {
-            jogadorPerto = true;
-        }
+        jogadorPerto = true;
+        Debug.Log("Colidiu");
     }
 
     // Este método é chamado quando o jogador sai do collider da placa
-    private void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        // Verificar se o jogador saiu do collider da placa
-        if (other.CompareTag("Player"))
-        {
-            jogadorPerto = false;
-        }
+        jogadorPerto = false;
+        Debug.Log("Saio Da Colisão");
     }
 }
